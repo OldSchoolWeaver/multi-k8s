cbcfe3455c28a82e0acea1e855a604089fc40a5c
OldSchoolWeaver <OldSchoolWeaver@gmail.com>
2022-07-02 18:40:48 +1000

    Bug Fix

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
+          <h1>Welcome to the FIB Calculator </h1>
           <img src={logo} className="App-logo" alt="logo" />
           <a
             className="App-link"
